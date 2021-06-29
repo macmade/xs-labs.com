228cda3c4bec4fd27abba6818c8ec8028d2300e2
macmade <macmade@xs-labs.com>
2021-06-29 22:07:34 +0200

    Notarize

--- en/apps/notarize/overview/index.php
+++ en/apps/notarize/overview/index.php
@@ -1,3 +1,17 @@
+<div>
+    <img src="/uploads/image/notarize/icon.png" alt="notarize" width="140" height="140" class="pull-right" />
+</div>
 <p>
-    Coming soon...
+    <strong>A notarization status monitoring tool for macOS, supporting multiple developer accounts.</strong>
 </p>
+<h3 class="clearer">Download &amp; Source Code</h3>
+<ul>
+    <li>Latest release: <a href="https://github.com/macmade/Notarize/releases/latest" title="Latest release">github.com/macmade/Notarize/releases/latest</a></li>
+    <li>Source code: <a href="https://github.com/macmade/Notarize" title="Source code">github.com/macmade/Notarize</a></li>
+</ul>
+<h3 class="clearer">Screenshots</h3>
+<div class="row">
+    <div class="col-xs-4">
+        <a href="/uploads/image/notarize/screenshot-full.png" class="lightbox"><img src="/uploads/image/notarize/screenshot.png" alt="" width="200" height="110" class="img-thumbnail" /></a>
+    </div>
+</div>
