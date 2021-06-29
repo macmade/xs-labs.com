cbc503dd0f46cf130797258ea48b6f116657ac4c
macmade <macmade@xs-labs.com>
2021-06-29 20:08:00 +0200

    Preparing for apps

---
+++ en/apps/index.php
@@ -0,0 +1,121 @@
+<h2 class="text-center">Freebies from XS-Labs</h2>
+<div class="alert alert-info text-center">
+    All OpenSource projects are currently hosted on <a href="http://github.com/macmade">GitHub</a>.<br />
+    <a href="http://github.com/macmade">Feel free to fork!</a>
+</div>
+<div class="marketing">
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/xclean' ); ?>"><img src="/uploads/image/xclean/icon.png" alt="Xclean" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/xclean' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/xclean' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/hot' ); ?>"><img src="/uploads/image/hot/icon.png" alt="Hot" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/hot' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/hot' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/iobrowser' ); ?>"><img src="/uploads/image/iobrowser/icon.png" alt="IOBrowser" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/iobrowser' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/iobrowser' ); ?>">Learn more »</a>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/sensors' ); ?>"><img src="/uploads/image/sensors/icon.png" alt="Sensors" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/sensors' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/sensors' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/silicon' ); ?>"><img src="/uploads/image/silicon/icon.png" alt="Silicon" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/silicon' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/silicon' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/qemu-manager' ); ?>"><img src="/uploads/image/qemu-manager/icon.png" alt="QEMU Manager" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/qemu-manager' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/qemu-manager' ); ?>">Learn more »</a>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/game-of-life' ); ?>"><img src="/uploads/image/game-of-life/icon.png" alt="Game Of Life" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/game-of-life' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/game-of-life' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/notarize' ); ?>"><img src="/uploads/image/notarize/icon.png" alt="Notarize" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/notarize' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/notarize' ); ?>">Learn more »</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <div>
+                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/ccache-gui' ); ?>"><img src="/uploads/image/ccache-gui/icon.png" alt="CCache GUI" width="140" height="140" /></a>
+            </div>
+            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/ccache-gui' ); ?></h3>
+            <p>
+                Coming soon...
+            </p>
+            <p>
+                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/ccache-gui' ); ?>">Learn more »</a>
+            </p>
+        </div>
+    </div>
+</div>
