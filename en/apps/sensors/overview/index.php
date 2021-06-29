33b8e0bee223d2bca9b9dda29503886b3df022a1
macmade <macmade@xs-labs.com>
2021-06-29 21:50:46 +0200

    Sensors

--- en/apps/sensors/overview/index.php
+++ en/apps/sensors/overview/index.php
@@ -1,3 +1,17 @@
+<div>
+    <img src="/uploads/image/sensors/icon.png" alt="Hot" width="140" height="140" class="pull-right" />
+</div>
 <p>
-    Coming soon...
+    <strong>A macOS application displaying the thermal, voltage and current sensor values.</strong>
 </p>
+<h3 class="clearer">Download &amp; Source Code</h3>
+<ul>
+    <li>Latest release: <a href="https://github.com/macmade/Sensors/releases/latest" title="Latest release">github.com/macmade/Sensors/releases/latest</a></li>
+    <li>Source code: <a href="https://github.com/macmade/Sensors" title="Source code">github.com/macmade/Sensors</a></li>
+</ul>
+<h3 class="clearer">Screenshots</h3>
+<div class="row">
+    <div class="col-xs-4">
+        <a href="/uploads/image/sensors/screenshot-full.png" class="lightbox"><img src="/uploads/image/sensors/screenshot.png" alt="" width="200" height="307" class="img-thumbnail" /></a>
+    </div>
+</div>
