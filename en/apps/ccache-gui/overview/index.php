9f70418d18b11c533c19509f65e2c081497569f4
macmade <macmade@xs-labs.com>
2021-06-29 22:11:29 +0200

    ccache-gui

--- en/apps/ccache-gui/overview/index.php
+++ en/apps/ccache-gui/overview/index.php
@@ -1,3 +1,24 @@
+<div>
+    <img src="/uploads/image/ccache-gui/icon.png" alt="ccache-gui" width="140" height="140" class="pull-right" />
+</div>
 <p>
-    Coming soon...
+    <strong>A GUI helper application for the ccache tool.</strong>
 </p>
+<p>
+    ccache can be installed on macOS with Homebrew:<br />
+    <code>brew install ccache</code>
+</p>
+<p>
+    The application run by default in the menu bar, but the window can be detached for convenience.
+</p>
+<h3 class="clearer">Download &amp; Source Code</h3>
+<ul>
+    <li>Latest release: <a href="https://github.com/macmade/ccache-gui/releases/latest" title="Latest release">github.com/macmade/ccache-gui/releases/latest</a></li>
+    <li>Source code: <a href="https://github.com/macmade/ccache-gui" title="Source code">github.com/macmade/ccache-gui</a></li>
+</ul>
+<h3 class="clearer">Screenshots</h3>
+<div class="row">
+    <div class="col-xs-4">
+        <a href="/uploads/image/ccache-gui/screenshot-full.png" class="lightbox"><img src="/uploads/image/ccache-gui/screenshot.png" alt="" width="200" height="208" class="img-thumbnail" /></a>
+    </div>
+</div>
