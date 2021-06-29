9906dd61e751acc16f626f31927c77e7eeff18f4
macmade <macmade@xs-labs.com>
2021-06-29 21:47:13 +0200

    Xclean

--- en/apps/xclean/overview/index.php
+++ en/apps/xclean/overview/index.php
@@ -1,3 +1,20 @@
+<div>
+    <img src="/uploads/image/xclean/icon.png" alt="Hot" width="140" height="140" class="pull-right" />
+</div>
 <p>
-    Coming soon...
+    <strong>Xclean is a macOS menu bar app that provides a convenient way to clear Xcode's derived data or module cache.</strong>
 </p>
+<p>
+    It also support automatic removal of derived data that is no longer associated with an existing project.
+</p>
+<h3 class="clearer">Download &amp; Source Code</h3>
+<ul>
+    <li>Latest release: <a href="https://github.com/macmade/Xclean/releases/latest" title="Latest release">github.com/macmade/Xclean/releases/latest</a></li>
+    <li>Source code: <a href="https://github.com/macmade/Xclean" title="Source code">github.com/macmade/Xclean</a></li>
+</ul>
+<h3 class="clearer">Screenshots</h3>
+<div class="row">
+    <div class="col-xs-4">
+        <a href="/uploads/image/xclean/screenshot-full.png" class="lightbox"><img src="/uploads/image/xclean/screenshot.png" alt="" width="200" height="307" class="img-thumbnail" /></a>
+    </div>
+</div>
