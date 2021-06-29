266e2f6250f3b8683fe646237874629d2fed4dec
macmade <macmade@xs-labs.com>
2021-06-29 22:15:11 +0200

    GOL

--- en/apps/game-of-life/overview/index.php
+++ en/apps/game-of-life/overview/index.php
@@ -1,3 +1,49 @@
+<div>
+    <img src="/uploads/image/game-of-life/icon.png" alt="Silicon" width="140" height="140" class="pull-right" />
+</div>
 <p>
-    Coming soon...
+    <strong>Explore Game Of Life, a cellular automaton created by John Conway in 1970 with this highly customisable open-source implementation.</strong>
 </p>
+<p>
+    The Game Of Life was created in 1970 by mathematician John Conway.
+</p>
+<p>
+    It consists of a two dimensional orthogonal grid of cells, each of which being alive or dead.<br />
+    Cells evolve at each turn following simple rules:
+</p>
+<ul>
+    <li>A live cell with fewer than two live neighbours dies.</li>
+    <li>A live cell with more than three live neighbours dies.</li>
+    <li>A dead cell with three live neighbours becomes alive.</li>
+</ul>
+<p>
+    <a href="https://itunes.apple.com/us/app/game-of-life/id1377718068?ls=1&mt=12" class="btn btn-primary" role="button">Download on the App Store</a>
+    <a href="https://github.com/macmade/GameOfLife" class="btn btn-default" role="button">View source on GitHub</a>
+</p>
+<h3 class="clearer">Screenshots</h3>
+<div class="row">
+    <div class="col-sm-3">
+        <div class="text-center">
+            <a href="/uploads/image/game-of-life/Screen-1.png" class="lightbox"><img src="/uploads/image/game-of-life/Screen-1.png" alt="Main Window" width="200" height="125" class="img-thumbnail"></a><br>
+            Main Window
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="text-center">
+            <a href="/uploads/image/game-of-life/Screen-2.png" class="lightbox"><img src="/uploads/image/game-of-life/Screen-2.png" alt="Preferences" width="200" height="125" class="img-thumbnail"></a><br>
+            Preferences
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="text-center">
+            <a href="/uploads/image/game-of-life/Screen-3.png" class="lightbox"><img src="/uploads/image/game-of-life/Screen-3.png" alt="Custom Rules" width="200" height="125" class="img-thumbnail"></a><br>
+            Custom Rules
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="text-center">
+            <a href="/uploads/image/game-of-life/Screen-4.png" class="lightbox"><img src="/uploads/image/game-of-life/Screen-4.png" alt="Pattern Viewer" width="200" height="125" class="img-thumbnail"></a><br>
+            Pattern Viewer
+        </div>
+    </div>
+</div>
