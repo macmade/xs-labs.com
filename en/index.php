533fc74f9387b7c9a6b81fb8f25c499594d14886
macmade <macmade@xs-labs.com>
2021-07-02 15:16:02 +0200

    Home

--- en/index.php
+++ en/index.php
@@ -1,33 +1,53 @@
-<h2 class="text-center">Featured projects</h2>
 <div class="marketing">
     <div class="row">
         <div class="col-sm-6">
-            <div>
-                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>"><img src="/uploads/image/xeos/icon-circle.png" alt="XEOS" width="140" height="140" /></a>
+            <h2 class="text-center">Featured Project</h2>
+            <hr />
+            <div class="row">
+                <div class="col-sm-12">
+                    <div>
+                        <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>"><img src="/uploads/image/xeos/icon-circle.png" alt="XEOS" width="140" height="140" /></a>
+                    </div>
+                    <h3><?php print \XS\Menu::getInstance()->getPageLink( '/projects/xeos' ); ?></h3>
+                    <p>
+                        XEOS is an experimental 32/64 bits Operating System for x86 platforms, written from scratch in Assembly and C.
+                        Its main purpose is educationnal, and to provide people interested in OS development with a clean code base.
+                    </p>
+                    <p>
+                        <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>">Learn more »</a>
+                    </p>
+                </div>
             </div>
-            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/projects/xeos' ); ?></h3>
-            <p>
-                XEOS is an experimental 32/64 bits Operating System for x86 platforms, written from scratch in Assembly and C.
-                It includes a C99 Standard Library, and aims at POSIX/SUS2 compatibility.
-                Its main purpose is educationnal, and to provide people interested in OS development with a clean code base.
-                While available only for x86, it may evolve to support other platforms.
-            </p>
-            <p>
-                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xeos' ); ?>">Learn more »</a>
-            </p>
         </div>
         <div class="col-sm-6">
-            <div>
-                <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>"><img src="/uploads/image/xsfoundation/icon-circle.png" alt="XSFoundation" width="140" height="140" /></a>
+            <h2 class="text-center">Latest Apps</h2>
+            <hr />
+            <div class="row">
+                <div class="col-sm-6">
+                    <div>
+                        <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/iobrowser' ); ?>"><img src="/uploads/image/iobrowser/icon.png" alt="IOBrowser" width="140" height="140" /></a>
+                    </div>
+                    <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/iobrowser' ); ?></h3>
+                    <p>
+                        IOBrowser is a macOS application for browsing the IOKit registry.
+                    </p>
+                    <p>
+                        <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/iobrowser' ); ?>">Learn more »</a>
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <div>
+                        <a href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/sensors' ); ?>"><img src="/uploads/image/sensors/icon.png" alt="Sensors" width="140" height="140" /></a>
+                    </div>
+                    <h3><?php print \XS\Menu::getInstance()->getPageLink( '/apps/sensors' ); ?></h3>
+                    <p>
+                        Sensors is a macOS application displaying the temperature and power.
+                    </p>
+                    <p>
+                        <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/apps/sensors' ); ?>">Learn more »</a>
+                    </p>
+                </div>
             </div>
-            <h3><?php print \XS\Menu::getInstance()->getPageLink( '/projects/xsfoundation' ); ?></h3>
-            <p>
-                The XSFoundation C Library provides the base for object-oriented C style coding, reference counting memory management with auto-release capabilities, reflection, runtime environment, polymorphism, exceptions, and basic objects.
-                While available as a standalone, cross-platform library, its main purpose is to be integrated in the XEOS Operating System.
-            </p>
-            <p>
-                <a class="btn btn-default" href="<?php print \XS\Menu::getInstance()->getPageUrl( '/projects/xsfoundation' ); ?>">Learn more »</a>
-            </p>
         </div>
     </div>
 </div>
